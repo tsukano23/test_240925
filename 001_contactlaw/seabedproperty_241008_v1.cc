707fe9d6489c73244aad0fd50112bdfb506ec553
tsukano <tsukano.fumitaka.699@gmail.com>
2024-10-08 19:56:58 +0900

    241008_v3

--- 001_contactlaw/seabedproperty_241008_v1.cc
+++ 001_contactlaw/seabedproperty_241008_v1.cc
@@ -8,21 +8,17 @@
 #include <limits>
 
 #include "mbconfig.h"
-//#include "ContactLaw.h"
+#include "seabedproperty_241008_v1.h"
 
-
-class seabedprop
+seabedprop::seabedprop(void)
 {
-private:
-    doublereal z_node;              //係留物のZ座標
-    doublereal z_seabed;            //海底面のZ座標             
-public:
-    seabedprop(void);
-    virtual ~seabedprop(void);
+    NO_OP;
+}
 
-    virtual void setValue(doublereal& sbz_node, doublereal& sbz_seabed);
-    virtual void get(doublereal& sbz_node, doublereal& sbz_seabed) const;
-};
+seabedprop::~seabedprop(void)
+{
+    NO_OP;
+}
 
 //各パラメータをメンバ変数としてセット
 void
@@ -38,4 +34,26 @@ seabedprop::get(doublereal& sbz_node, doublereal& sbz_seabed) const
 {
     sbz_node = z_node;
     sbz_seabed = z_seabed;
+}
+
+seabedpropowner::seabedpropowner(void)
+{
+    NO_OP;
+}
+
+seabedpropowner::~seabedpropowner(void)
+{
+    NO_OP;   
+}
+
+void
+seabedpropowner::setSeabedprop(doublereal& z_node, doublereal& z_seabed)
+{
+    pSeabedprop.setValue(z_node,z_seabed);
+}
+
+void
+seabedpropowner::get(doublereal& z_node, doublereal& z_seabed) const
+{
+    pSeabedprop.get(z_node,z_seabed);
 }
--- 001_contactlaw/seabedproperty_241008_v1.h
+++ 001_contactlaw/seabedproperty_241008_v1.h
@@ -1,28 +1,38 @@
 
-#ifndef CONTACTLAW_H
-#define CONTACTLAW_H
+#ifndef SEABEDPROP_H
+#define SEABEDPROP_H
 
-#include <mbconfig.h>
+#include <mbconfiz_seabed.h>
 #include "dataman.h"
 
-/* =================================================
- * class ContactLaw
- * ================================================= */
-class ContactLaw
+class seabedprop
 {
-public:
-    ContactLaw(void);
-    ~ContactLaw(void);
-
-    //set private variable
-    virtual void setSeaDepth(Vec3 value_SeaDepth) const;
-    virtual void setNodeHeight(Vec3 value_NodeHeight) const;
-
-    //calculate strain and forces
-    virtual void Distinction(doublereal D1) const;
 private:
-    mutable Vec3 D;
-    mutable Vec3 H;
+    doublereal z_node;              //係留物のZ座標
+    doublereal z_seabed;            //海底面のZ座標             
+public:
+    seabedprop(void);
+    virtual ~seabedprop(void);
+
+    virtual void setValue(doublereal& sbz_node, doublereal& sbz_seabed);
+    virtual void z_seabedet(doublereal& sbz_node, doublereal& sbz_seabed) const;
 };
 
-#endif //ContactLaw_H
+#endif // SEABEDPROP_H
+
+#ifndef SEABEDPROPOWNER_H
+#define SEABEDPROPOWNER_H
+
+class seabedpropowner
+{
+protected:
+    seabedprop pSeabedprop; 
+public:
+    seabedpropowner(void);
+    virtual ~seabedpropowner(void);
+
+    virtual void setSeabedprop(doublereal& z_node, doublereal& z_seabed);
+    virtual void get(doublereal& z_node, doublereal& z_seabed) const;
+};
+
+#endif // SEABEDPROPOWBER_H
